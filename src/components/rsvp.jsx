eee135a3278e17f7500ac375c33e88df7ba14411
Eric Adamski <er.adamski@gmail.com>
2017-06-05 21:00:23 -0400

    env

--- src/components/rsvp.jsx
+++ src/components/rsvp.jsx
@@ -13,12 +13,12 @@ import Container from './container.jsx';
 import Information from './correct-information.jsx';
 
 const config = {
-  apiKey: process.env.FIRE_API_KEY,
-  authDomain: process.env.FIRE_AUTH_DOMAIN,
-  databaseURL: process.env.FIRE_DATABASE_URL,
-  projectId: process.env.FIRE_PROJECT_ID,
-  storageBucket: process.env.FIRE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIRE_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_FIRE_API_KEY,
+  authDomain: process.env.REACT_APP_FIRE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIRE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIRE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIRE_MESSAGING_SENDER_ID,
 };
 
 firebase.initializeApp(config);
