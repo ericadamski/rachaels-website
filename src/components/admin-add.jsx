4346589850f38842220a2660c612e24cd87c52bb
Eric Adamski <er.adamski@gmail.com>
2017-06-05 22:30:33 -0400

    multi ppl in admin

--- src/components/admin-add.jsx
+++ src/components/admin-add.jsx
@@ -7,79 +7,90 @@ import firebase from 'firebase';
 import './admin-add.css';
 
 export default class Add extends React.Component {
-  state = { showOther: false };
+  state = { members: [{}] };
+
+  members = [{}];
 
   submit = this.submit.bind(this);
+  add = this.add.bind(this);
+
+  add() {
+    this.setState(({ members }) => {
+      members.push({});
+      this.members = members;
+
+      return { members };
+    });
+  }
 
   submit() {
-    const name = this.name.getValue();
     const email = this.email.getValue();
-    const vegan = this.vegan.isChecked();
-    const vegetarian = this.vegetarian.isChecked();
-    const lactose = this.lactose.isChecked();
-    const gluten = this.gluten.isChecked();
-    const other = this.other.getValue();
 
-    if (email === '' || name === '') return;
+    const members = this.members.map(member => ({
+      name: member.nameRef.getValue(),
+      diet: {
+        vegan: member.veganRef.isChecked(),
+        vegetarian: member.vegetarianRef.isChecked(),
+        'lactose free': member.lactoseRef.isChecked(),
+        'gluten free': member.glutenRef.isChecked(),
+        other: member.otherRef.getValue(),
+      },
+    }));
+
+    if (email === '') return;
 
     firebase
       .database()
       .ref('attendees')
       .push({
-        name,
         email,
-        diet: {
-          vegan,
-          vegetarian,
-          'lactose free': lactose,
-          'gluten free': gluten,
-          other,
-        },
+        members,
         address: '',
       })
       .then(() => {
-        this.name.input.value = '';
-        this.email.input.value = '';
-        this.other.input.value = '';
-        this.vegan.setChecked(false);
-        this.vegetarian.setChecked(false);
-        this.lactose.setChecked(false);
-        this.gluten.setChecked(false);
+        this.email.input.value = null;
+        this.members = [{}];
+        this.setState({ members: [{}] });
       });
   }
 
   render() {
     return (
             <form className="add-form" onSubmit={this.submit}>
-                <TextField
-                    ref={n => this.name = n}
-                    floatingLabelText="name"
-                    type="text"
-                />
-                <TextField
-                    ref={n => this.email = n}
-                    floatingLabelText="email"
-                    type="email"
-                />
-                <Checkbox ref={n => this.vegan = n} label="Vegan" />
-                <Checkbox ref={n => this.vegetarian = n} label="Vegetarian" />
-                <Checkbox ref={n => this.lactose = n} label="Lactose Free" />
-                <Checkbox ref={n => this.gluten = n} label="Gluten Free" />
-                <Checkbox
-                    onCheck={e => {
-                      console.log(e);
-                      this.setState({ showOther: e.target.checked });
-                    }}
-                    label="Other"
-                />
-                <div className={!this.state.showOther ? 'hidden' : ''}>
-                    <TextField
-                        ref={n => this.other = n}
-                        floatingLabelText="Extra Dietary Needs"
-                        multiLine
-                    />
-                </div>
+              <TextField
+                  ref={n => this.email = n}
+                  floatingLabelText="email"
+                  type="email"
+              />
+              { this.state.members.map((member, i) => {
+                return (
+                    <div key={i}>
+                      <TextField
+                          ref={n => this.members[i].nameRef = n}
+                          floatingLabelText="name"
+                          type="text"
+                      />
+                      <Checkbox ref={n => this.members[i].veganRef = n} label="Vegan" />
+                      <Checkbox ref={n => this.members[i].vegetarianRef = n} label="Vegetarian" />
+                      <Checkbox ref={n => this.members[i].lactoseRef = n} label="Lactose Free" />
+                      <Checkbox ref={n => this.members[i].glutenRef = n} label="Gluten Free" />
+                      <Checkbox
+                          onCheck={e => this.setState({ [i]: e.target.checked })}
+                          label="Other"
+                      />
+                      <div className={!this.state[i] ? 'hidden' : ''}>
+                          <TextField
+                              ref={n => this.members[i].otherRef = n}
+                              floatingLabelText="Extra Dietary Needs"
+                              multiLine
+                          />
+                      </div>
+                    </div>
+                    );
+              })
+                }
                 <RaisedButton onClick={this.submit} label="Save" />
+                <RaisedButton onClick={this.add} label="Add" />
             </form>
         );
   }
